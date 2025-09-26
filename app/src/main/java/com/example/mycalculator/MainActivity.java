8f4d43458d400fa4ecd9b9b0133ea89432e8de40
Idan Frenkel <ifren039@uottawa.ca>
2025-09-25 20:54:08 -0400

    core functionality complete, see comments on line 148 in MainActivity.java for remarks

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mycalculator;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,34 +11,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.math.BigDecimal;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_clear;
+    Button btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_decimal, btn_plus, btn_subtract, btn_multiply, btn_divide, btn_clear, btn_equal;
 
     TextView text_display;
 
-    @SuppressLint("MissingInflatedId")
+    // To evaluate math expression
+    ScriptEngine engine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        engine = new ScriptEngineManager().getEngineByName("rhino");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        setClickListeners();
-
+        btn_0 = (Button) findViewById(R.id.btn_0);
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_5 = (Button) findViewById(R.id.btn_5);
+        btn_6 = (Button) findViewById(R.id.btn_6);
+        btn_7 = (Button) findViewById(R.id.btn_7);
+        btn_8 = (Button) findViewById(R.id.btn_8);
+        btn_9 = (Button) findViewById(R.id.btn_9);
+        btn_decimal = (Button) findViewById(R.id.btn_decimal);
         btn_clear = (Button) findViewById(R.id.btn_clear);
-
+        btn_plus = (Button) findViewById(R.id.btn_plus);
+        btn_subtract = (Button) findViewById(R.id.btn_subtract);
+        btn_multiply = (Button) findViewById(R.id.btn_multiply);
+        btn_divide = (Button) findViewById(R.id.btn_divide);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
         text_display = (TextView) findViewById(R.id.textview_input_display);
 
-
-
-
+        setClickListeners();
     }
     private void setClickListeners(){
+        btn_0.setOnClickListener(this);
         btn_1.setOnClickListener(this);
         btn_2.setOnClickListener(this);
         btn_3.setOnClickListener(this);
@@ -50,7 +72,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_8.setOnClickListener(this);
         btn_9.setOnClickListener(this);
         btn_clear.setOnClickListener(this);
+        btn_plus.setOnClickListener(this);
+        btn_subtract.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
         switch(v.getId()){
@@ -84,13 +112,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_clear:
                 clear_display();
                 break;
-
+            case R.id.btn_plus:
+                addNumber("+");
+                break;
+            case R.id.btn_subtract:
+                addNumber("-");
+                break;
+            case R.id.btn_divide:
+                addNumber("/");
+                break;
+            case R.id.btn_multiply:
+                addNumber("*");
+                break;
+            case R.id.btn_equal:
+                String result = null;
+                try {
+                    result = evaluate(text_display.getText().toString());
+                    text_display.setText(result);
+                } catch (ScriptException e) {
+                    text_display.setText("Error");
+                }
+                text_display.setText(result);
+                break;
         }
     }
+
     private void addNumber(String number){
         text_display.setText(text_display.getText() + number);
     }
+
     private void clear_display(){
         text_display.setText("");
+    }
+
+    // uncaught use case: when invalid operands are included, returns nothing
+    // maybe an alert is in order, or to setText for text_display as "Error"
+    private String evaluate(String expression) throws ScriptException {
+        String result = engine.eval(expression).toString();
+        BigDecimal decimal = new BigDecimal(result);
+        return decimal.setScale(2, BigDecimal.ROUND_HALF_UP).toString();
     }
 }
